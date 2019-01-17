d9a8b2dca13085219b9ef1081f5565d527093616
pras75299 <singh.prashantking@gmail.com>
2019-01-18 00:41:19 +0530

    added remove function

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ const tweetList = document.getElementById('tweet-list');
 //event listeners
 eventListeners();
 function eventListeners() {
-    document.querySelector('#form').addEventListener('submit', newTweet)
+    document.querySelector('#form').addEventListener('submit', newTweet);
+
+    //remove the tweet from the list
+    tweetList.addEventListener('click', removeTweet);
 }
 
 
@@ -21,11 +24,29 @@ function newTweet(e) {
     const tweet = document.getElementById('tweet').value;
 
     //create the remove button
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
 
     //Create an element
     const li = document.createElement('li');
     li.textContent = tweet;
+
+    //Add the remove btn to each tweet
+
+    li.appendChild(removeBtn);
+
+    //add to the list
+
     tweetList.appendChild(li);
+}
+
+//Remove the tweets from the dom
+
+function removeTweet(e) {
+    if(e.target.classList.contains('remove-tweet')) {
+        e.target.parentElement.remove();
+    }
 }
 
 
